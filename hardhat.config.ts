f06339b6b75fa9338384efa779150ca7f085a2f0
tyrannojung <tyranno2020@gmail.com>
2024-09-29 22:27:00 +0900

    fix: update Hardhat config to set correct sources path for solady contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,28 +1,33 @@
-import 'hardhat-deploy';
+import "hardhat-deploy";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
-import 'dotenv/config';
-import { HardhatUserConfig } from 'hardhat/config';
+import "dotenv/config";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: {
-    compilers: [{
-      version: '0.8.15',
-      settings: {
-        optimizer: { enabled: true, runs: 1000000 }
-      }
-    }],
+    compilers: [
+      {
+        version: "0.8.15",
+        settings: {
+          optimizer: { enabled: true, runs: 1000000 },
+        },
+      },
+    ],
   },
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
     arbitrumSepolia: {
       url: process.env.ARBITRUM_SEPOLIA_URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-    }
-  }
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  paths: {
+    sources: "./contracts/P256/solady/src",
+  },
 };
 
 export default config;
